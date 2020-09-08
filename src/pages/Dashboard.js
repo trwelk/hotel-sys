5b274b4d6e9fcdd6e69d96d455e8dea032453ebc
Avantha <avanthac@yahoo.com>
2020-09-08 23:20:02 +0530

    Adding new features

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -14,6 +14,7 @@ import ReservatonBoxView from '../components/frontOffice/reservation/Reservation
 import Finance from '../components/frontOffice/Finance';
 import Editable from '../components/frontOffice/rooms/Editable';
 import fnbProdMgmt from '../components/F&bProduction/management/fnbProdMgmt';
+import AddMenu from '../components/F&bProduction/management/AddNewMenu';
 
 function Copyright() {
   return (
@@ -202,6 +203,7 @@ function Dashboard(props) {
             <Route exact path='/res' component={ReservatonBoxView}/>
             <Route exact path='/ed' component={Editable}/>
             <Route exact path='/Food & Beverages' component={fnbProdMgmt}/>
+            <Route exact path='/newMenu' component={AddMenu}/>
             </Switch>
 
 
