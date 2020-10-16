d26bda5c0a9efb7b0030740ee8a3184adaf824d9
trwelk <trewon.weerasooriya@ifs.com>
2020-10-16 20:26:10 +0530

    status collumn added

--- src/redux/actions/PnIActions/productHandler.js
+++ src/redux/actions/PnIActions/productHandler.js
@@ -1,51 +1,52 @@
-// export const updateProduct = (payload,pType,priority,sName) => {
-//     console.log(payload)
-//     return (dispatch,getState,{getFirestore,getFirebase}) => {
-//         const firestore = getFirestore();
-//         firestore.collection("productMng").doc(payload.oId).update({
-//             oId:payload.oId,
-//             sName:payload.sName,
-//             pType:payload.pType,
-//             priority:payload.priority,
-//             qty:payload.qty,
-//             date:payload.date
-//             //status:payload.status
-//         }); 
-//     }
+export const updateProduct = (payload,pType,priority,sName) => {
+    console.log(payload)
+    return (dispatch,getState,{getFirestore,getFirebase}) => {
+        const firestore = getFirestore();
+        firestore.collection("productMng").doc(payload.oId).update({
+            oId:payload.oId,
+            sName:payload.sName,
+            pType:payload.pType,
+            priority:payload.priority,
+            qty:payload.qty,
+            date:payload.date,
+            status:payload.status
+        }); 
+    }
 
-// }
+}
 
-// export const insertProduct = (payload,pType,priority,sName) => {
-//     console.log(payload)
-//     return (dispatch,getState,{getFirestore,getFirebase}) => {
-//         const firestore = getFirestore();
-//         firestore.collection('productMng').doc(payload.oId).set({
-//             oId:payload.oId,
-//             sName:sName,
-//             pType:pType,
-//             department:payload.department,
-//             priority:priority,
-//             qty:payload.qty,
-//             date:payload.date
-//         }).then((response) => {
-//             console.log(response)
-//         }).catch((response) => {
-//             console.log(response)
-//         })
-//     }
+export const insertProduct = (payload,pType,priority,sName) => {
+    console.log(payload)
+    return (dispatch,getState,{getFirestore,getFirebase}) => {
+        const firestore = getFirestore();
+        firestore.collection('productMng').doc(payload.oId).set({
+            oId:payload.oId,
+            sName:sName,
+            pType:pType,
+            department:payload.department,
+            priority:priority,
+            qty:payload.qty,
+            date:payload.date,
+            status:"PENDING",
+        }).then((response) => {
+            console.log(response)
+        }).catch((response) => {
+            console.log(response)
+        })
+    }
 
-// }
+}
 
-// export const deleteProduct = (productId) => {
-//     console.log(productId)
-//     return (dispatch,getState,{getFirestore,getFirebase}) => {
-//         const firestore = getFirestore();
-//         firestore.collection('productMng').doc(productId).delete()
-//             .then((response) => {
-//                 console.log(response)
-//             }).catch((error) => {
-//                 console.log(error)
-//             })
-//     }
+export const deleteProduct = (productId) => {
+    console.log(productId)
+    return (dispatch,getState,{getFirestore,getFirebase}) => {
+        const firestore = getFirestore();
+        firestore.collection('productMng').doc(productId).delete()
+            .then((response) => {
+                console.log(response)
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
 
-// }
+}
