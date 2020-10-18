264b2dd359d73240dcec538f72b6307e6f1c034f
trwelk <trewon.weerasooriya@ifs.com>
2020-10-18 15:41:58 +0530

    Validation for house keeping

--- src/components/hr/absence/AbsenceForm.js
+++ src/components/hr/absence/AbsenceForm.js
@@ -169,7 +169,19 @@ function AbsenceRequest(props) {
       return null
   }
 
+  const getFormattedDate = (date) => {
+    var year = date.getFullYear();
+    
   
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+  
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return month + '/' + day + '/' + year;
+  }
+
 
   const handleClose = () => {
     setState({ ...state, open: false });
