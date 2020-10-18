72dd505be557ede1f0546a5974bd83a40a8be7d2
Pasindu <sandungwp@gmail.com>
2020-10-18 14:00:47 +0530

    new

--- src/components/fnbServices/BarChart.js
+++ src/components/fnbServices/BarChart.js
@@ -1,60 +1,114 @@
-import { BorderColor } from '@material-ui/icons';
-import React from 'react';
-import {Bar} from 'react-chartjs-2'
-import { firestoreConnect, isLoaded } from 'react-redux-firebase';
-import { useSelector, connect } from 'react-redux';
-// import { Bar } from 'recharts';
+// import { BorderColor } from '@material-ui/icons';
+// import React from 'react';
+// import {Bar} from 'react-chartjs-2'
+// import { firestoreConnect, isLoaded } from 'react-redux-firebase';
+// import { useSelector, connect } from 'react-redux';
+// // import { Bar } from 'recharts';
 
-function BarChart () {
-//     const [state, setState] = React.useState({
-//         open: false,
-//         vertical: 'bottom',
-//         horizontal: 'right',
-//       });
-//       const { vertical, horizontal, open, error } = state;
-//     const feedbacks = useSelector(state => state.firestore.ordered.product)
-//   const datas = feedbacks ? (feedbacks.map(feedback => ({ ...feedback }))) : (null)
-    const data = {
-        // labels: [...datas.ProName,],
-        labels: ['rice','hooper','kott','name'],
-        datasets:[
-            {
-                label: 'stock quantity',
-                // data:[...datas.price],
-                data:[3,5,6,7],
-                borderColor:['rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)'],
-                backgroundColor:['rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)'],
-                // pointBackgroundColor:'rgba(255,206,86,0.2)',
-                // pointBorderColor:'rgba(255,206,86,0.2)'
-            },
-            {
-                label: 'sell quantity',
-                data:[5,8,3,5],
-                borderColor:['rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)'],
-                backgroundColor:['rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)'],
-                // pointBackgroundColor:'rgba(54,162,235,0.2)',
-                // pointBorderColor:'rgba(54,162,235,0.2)'
-            }
-        ]
-    }
-    const options = {
-        title:{
-            display:true,
-            text:'bar chart'
-        },
-        scales:{
-            yAxes:[
-                {
-                    ticks:{
-                        min:0,
-                        max:9,
-                        stepSize:1
-                    }
-                }    
-            ]
+// function BarChart () {
+// //     const [state, setState] = React.useState({
+// //         open: false,
+// //         vertical: 'bottom',
+// //         horizontal: 'right',
+// //       });
+// //       const { vertical, horizontal, open, error } = state;
+// //     const feedbacks = useSelector(state => state.firestore.ordered.product)
+// //   const datas = feedbacks ? (feedbacks.map(feedback => ({ ...feedback }))) : (null)
+//     const data = {
+//         // labels: [...datas.ProName,],
+//         labels: ['rice','hooper','kott','name'],
+//         datasets:[
+//             {
+//                 label: 'stock quantity',
+//                 // data:[...datas.price],
+//                 data:[3,5,6,7],
+//                 borderColor:['rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)'],
+//                 backgroundColor:['rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)','rgba(255,206,86,0.2)'],
+//                 // pointBackgroundColor:'rgba(255,206,86,0.2)',
+//                 // pointBorderColor:'rgba(255,206,86,0.2)'
+//             },
+//             {
+//                 label: 'sell quantity',
+//                 data:[5,8,3,5],
+//                 borderColor:['rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)'],
+//                 backgroundColor:['rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)','rgba(54,162,235,0.2)'],
+//                 // pointBackgroundColor:'rgba(54,162,235,0.2)',
+//                 // pointBorderColor:'rgba(54,162,235,0.2)'
+//             }
+//         ]
+//     }
+//     const options = {
+//         title:{
+//             display:true,
+//             text:'bar chart'
+//         },
+//         scales:{
+//             yAxes:[
+//                 {
+//                     ticks:{
+//                         min:0,
+//                         max:9,
+//                         stepSize:1
+//                     }
+//                 }    
+//             ]
+//         }
+//     }
+//     return <Bar data={data} options={options}/>
+// }
+
+// export default BarChart
+
+import React from 'react'
+import { Bar, Line, Pie } from 'react-chartjs-2'
+import { firestoreConnect } from 'react-redux-firebase';
+import { useSelector, connect } from 'react-redux';
+import { compose } from 'redux';
+
+function BarChart() {
+
+    // const fnbInv = useSelector(state => state.firestore.ordered.inventoryProducts)
+    // const Invdata = fnbInv ? (fnbInv.map(fnb_Inv => ({...fnb_Inv}))) : (null)
+    const Stock = useSelector(state => state.firestore.ordered.barInventory)
+    const AStock = Stock ? (Stock.map(Stocks => ({...Stocks}))) : (null)
+ 
+        let labels = [];
+        let data = [];
+        let borderColor = [];
+        let backgroundColor = [];
+    
+    if(AStock){
+        // let  st = AStock.filter(Stocks => Stocks.date >);
+        for (let index = 0; index < AStock.length; index++) {
+            labels.push(AStock[index].itemName);
+            data.push(AStock[index].qty); 
+            borderColor.push('rgba(255,206,86,0.2)');
+            backgroundColor.push('rgba(255,206,86,0.2)');
         }
     }
-    return <Bar data={data} options={options}/>
+        const chartData = {
+            labels:labels,
+            datasets:[{
+                label:'Bar Stock Analysis',
+                data: data,
+                borderColor:borderColor,
+                backgroundColor:backgroundColor
+            }]
+        }
+        return(
+            <div className="chart" id="barChart">
+                <Bar
+                    data={chartData}
+                    width={100}
+                    height={200}
+                    options={{
+                    maintainAspectRatio: false}}/>
+            </div>
+        )
+
 }
 
-export default BarChart
+  export default compose(connect(null,null),firestoreConnect([
+    {collection: 'barInventory'}
+  ])) (BarChart)
+
--- src/redux/actions/FnBServiceActions/BarInventoryAction.js
+++ src/redux/actions/FnBServiceActions/BarInventoryAction.js
@@ -1,15 +1,28 @@
+// export const  formatDate = (string) => {
+//     var options = { year: 'numeric', month: 'long', day: 'numeric' };
+//     return new Date(string).toLocaleDateString([],options);
+// }
 export const updateBarProduct = (payload) => {
     console.log(payload)
+    // let date;
+    // if(payload.date != null || payload.date != ""){
+    //     date = formatDate(payload.date);
+    // }
+    // else{
+    //     date = null;
+    // }
     return (dispatch, getState, { getFirestore, getFirebase }) => {
         const firestore = getFirestore();
-        payload.BIID = payload.id + payload.BarInvNo;
-        firestore.collection("inventoryProducts").doc(payload.BIID).update({
+        // payload.BIID = payload.id + payload.BarInvNo;
+        firestore.collection("barInventory").doc(payload.BarInvNo).update({
             BarInvNo:payload.BarInvNo,
-            BIID:payload.BIID,
-            id:payload.id,
             itemName:payload.itemName,
+            id:payload.id, 
             qty:payload.qty,
             price:payload.price,
+            date:payload.date
+            // date:(new Date()).toDateString()
+            // lastModified:payload.lastModified
             // tableNO: payload.tableNO,
             // room: payload.room,
             // description: payload.description,
@@ -22,10 +35,14 @@ export const updateBarProduct = (payload) => {
 
 export const insertBarProduct = (payload) => {
     console.log(payload)
+//     payload.date = (new Date()).toDateString();
+//     if(payload.date != null || payload.date != ""){
+//         payload.date = formatDate(payload.date);
+//    }
     return (dispatch, getState, { getFirestore, getFirebase }) => {
         const firestore = getFirestore();
-        payload.BIID = payload.id + payload.BarInvNo;
-        firestore.collection('inventoryProducts').doc(payload.BIID).set({
+        // payload.BIID = payload.id + payload.BarInvNo;
+        firestore.collection('barInventory').doc(payload.BarInvNo).set({
             ...payload
         }).then((response) => {
             console.log(response)
@@ -40,7 +57,7 @@ export const deleteBarProduct = (InvId) => {
     console.log(InvId)
     return (dispatch, getState, { getFirestore, getFirebase }) => {
         const firestore = getFirestore();
-        firestore.collection('inventoryProducts').doc(InvId.BIID).delete()
+        firestore.collection('barInventory').doc(InvId).delete()
             .then((response) => {
                 console.log(response)
             }).catch((error) => {
@@ -49,49 +66,149 @@ export const deleteBarProduct = (InvId) => {
     }
 
 }
-export const insertBarInvRec = (payload) => {
-    console.log(payload)
-    return (dispatch,getState,{getFirestore,getFirebase}) => {
-        const firestore = getFirestore();
-        firestore.collection('barInventory').doc(payload.BarInvNo).set({
-            ...payload
-        }).then((response) => {
-            console.log(response)
-        }).catch((response) => {
-            console.log(response)
-        })
-    }
+// export const insertBarInvRec = (payload) => {
+//     console.log(payload)
+//     return (dispatch,getState,{getFirestore,getFirebase}) => {
+//         const firestore = getFirestore();
+//         firestore.collection('barInventory').doc(payload.BarInvNo).set({
+//             ...payload
+//         }).then((response) => {
+//             console.log(response)
+//         }).catch((response) => {
+//             console.log(response)
+//         })
+//     }
 
-}
-export const updateBarInvRec = (payload) => {
-    console.log(payload)
-    return (dispatch,getState,{getFirestore,getFirebase}) => {
-        const firestore = getFirestore();
-        firestore.collection('barInventory').doc(payload.id).update({
-            id:payload.id,
-            itemName:payload.itemName,
-            price:payload.price,
-            qty:payload.qty,
-            expDate:payload.expDate,
-            lastModified:payload.lastModified,
-            stkStatus:payload.stkStatus
-        });
-    }
+// }
+// export const updateBarInvRec = (payload) => {
+//     console.log(payload)
+//     return (dispatch,getState,{getFirestore,getFirebase}) => {
+//         const firestore = getFirestore();
+//         firestore.collection('barInventory').doc(payload.id).update({
+//             id:payload.id,
+//             itemName:payload.itemName,
+//             price:payload.price,
+//             qty:payload.qty,
+//             expDate:payload.expDate,
+//             lastModified:payload.lastModified,
+//             stkStatus:payload.stkStatus
+//         });
+//     }
 
-}
+// }
 
 
 
-export const deleteBarInvRec = (InventoryId) => {
-    console.log(InventoryId)
-    return (dispatch,getState,{getFirestore,getFirebase}) => {
-        const firestore = getFirestore();
-        firestore.collection('barInventory').doc(InventoryId).delete()
-            .then((response) => {
-                console.log(response)
-            }).catch((error) => {
-                console.log(error)
-            })
-    }
+// export const deleteBarInvRec = (InventoryId) => {
+//     console.log(InventoryId)
+//     return (dispatch,getState,{getFirestore,getFirebase}) => {
+//         const firestore = getFirestore();
+//         firestore.collection('barInventory').doc(InventoryId).delete()
+//             .then((response) => {
+//                 console.log(response)
+//             }).catch((error) => {
+//                 console.log(error)
+//             })
+//     }
 
-}
+// }
+
+
+//----------------------------
+// export const updateBarProduct = (payload) => {
+//     console.log(payload)
+//     return (dispatch, getState, { getFirestore, getFirebase }) => {
+//         const firestore = getFirestore();
+//         payload.BIID = payload.id + payload.BarInvNo;
+//         firestore.collection("inventoryProducts").doc(payload.BIID).update({
+//             BarInvNo:payload.BarInvNo,
+//             BIID:payload.BIID,
+//             id:payload.id,
+//             itemName:payload.itemName,
+//             qty:payload.qty,
+//             price:payload.price,
+//             // tableNO: payload.tableNO,
+//             // room: payload.room,
+//             // description: payload.description,
+//             // status: payload.status
+//             // DateAndTime:payload.dateAndTime,
+//             // orId:payload.orId,
+//         });
+//     }
+// }
+
+// export const insertBarProduct = (payload) => {
+//     console.log(payload)
+//     return (dispatch, getState, { getFirestore, getFirebase }) => {
+//         const firestore = getFirestore();
+//         payload.BIID = payload.id + payload.BarInvNo;
+//         firestore.collection('inventoryProducts').doc(payload.BIID).set({
+//             ...payload
+//         }).then((response) => {
+//             console.log(response)
+//         }).catch((response) => {
+//             console.log(response)
+//         })
+//     }
+
+// }
+
+// export const deleteBarProduct = (InvId) => {
+//     console.log(InvId)
+//     return (dispatch, getState, { getFirestore, getFirebase }) => {
+//         const firestore = getFirestore();
+//         firestore.collection('inventoryProducts').doc(InvId.BIID).delete()
+//             .then((response) => {
+//                 console.log(response)
+//             }).catch((error) => {
+//                 console.log(error)
+//             })
+//     }
+
+// }
+// export const insertBarInvRec = (payload) => {
+//     console.log(payload)
+//     return (dispatch,getState,{getFirestore,getFirebase}) => {
+//         const firestore = getFirestore();
+//         firestore.collection('barInventory').doc(payload.BarInvNo).set({
+//             ...payload
+//         }).then((response) => {
+//             console.log(response)
+//         }).catch((response) => {
+//             console.log(response)
+//         })
+//     }
+
+// }
+// export const updateBarInvRec = (payload) => {
+//     console.log(payload)
+//     return (dispatch,getState,{getFirestore,getFirebase}) => {
+//         const firestore = getFirestore();
+//         firestore.collection('barInventory').doc(payload.id).update({
+//             id:payload.id,
+//             itemName:payload.itemName,
+//             price:payload.price,
+//             qty:payload.qty,
+//             expDate:payload.expDate,
+//             lastModified:payload.lastModified,
+//             stkStatus:payload.stkStatus
+//         });
+//     }
+
+// }
+
+
+
+// export const deleteBarInvRec = (InventoryId) => {
+//     console.log(InventoryId)
+//     return (dispatch,getState,{getFirestore,getFirebase}) => {
+//         const firestore = getFirestore();
+//         firestore.collection('barInventory').doc(InventoryId).delete()
+//             .then((response) => {
+//                 console.log(response)
+//             }).catch((error) => {
+//                 console.log(error)
+//             })
+//     }
+
+// }
